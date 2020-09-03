35631de68a2fba9d344d5d097e43b1a9b5c4efa9
Montarious Starks <montarious@usf.edu>
2020-09-03 03:23:21 -0400

    Commit the change

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VisualStudio2019_CSharpeGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Here is a change");
+        }
+    }
+}
